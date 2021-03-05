9a68247fe5c1e26a5c6dedd22e0d01ddfd9a1ba8
yuehaiming <915873514@qq.com>
2021-03-05 22:28:08 +0800

    teleport 测试

--- src/view/Page1.vue
+++ src/view/Page1.vue
@@ -1,10 +1,15 @@
 <template>
-    <h1>Page1</h1>
+    <div>
+        <h1>Page1</h1>
+        <Message />
+    </div>
 </template>
 
 <script>
+    import Message from "../components/Message.vue";
     export default {
-        name: "Page1"
+        name: "Page1",
+        components: {Message}
     }
 </script>
 
